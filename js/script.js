7f7d56aef2a061c86d34085c6029eac4fcb9343e
MAHBUBUL ALAM <33549330+promahbubul@users.noreply.github.com>
2023-08-20 08:19:19 +0600

    complate

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ function cardClicked(cardNumber) {
     const count = itemCartContainer.childElementCount
     // step-4
     const p = document.createElement('p')
-    p.innerHTML = `<p class="text-xl font-medium">${count+1}. ${productTitle}</p>`  
+    p.innerHTML = `<p class="text-xl font-medium">${count + 1}. ${productTitle}</p>`
     itemCartContainer.appendChild(p)
     // // step-5
     const productPriceElement = card[cardNumber].childNodes[3].childNodes[7].childNodes[1]
@@ -31,52 +31,66 @@ function cardClicked(cardNumber) {
     const totalPrice = elementNumber('total-price')
     // step-7
     const newTotalPrice = totalPrice + productPrice
+    const totalPriceFix = newTotalPrice.toFixed(2)
     const prevBalance = element('total-price')
-    prevBalance.innerText = newTotalPrice
+    prevBalance.innerText = totalPriceFix
 
     const pay = element('pay')
-    pay.innerText = newTotalPrice
+    pay.innerText = totalPriceFix
 
 
-   
-    
-    if(newTotalPrice >= 1000) {
+
+
+    if (newTotalPrice >= 1000) {
         const cuponButton = document.getElementById('cupon-btn');
         cuponButton.removeAttribute('disabled')
     }
 
- 
-    if(newTotalPrice > 0) {
+
+    if (newTotalPrice > 0) {
         const payBtn = element('pay-btn');
         payBtn.removeAttribute('disabled')
     }
-    
-    
+
+
 }
 
 
-document.getElementById('cupon-btn').addEventListener('click', function(){
-        const cuponField = element('cupon-field').value;
-        const cuponCode = element('cupon-code').innerText;
-        if(cuponField == cuponCode) {
-            const totalPrice = elementNumber('total-price')
-            const discountAmount = (totalPrice * 20) / 100;
-            const discountElement = element('discount')
-            discountElement.innerText = discountAmount
+document.getElementById('cupon-btn').addEventListener('click', function () {
+    const cuponField = element('cupon-field').value;
+    const cuponCode = element('cupon-code').innerText;
+    if (cuponField == cuponCode) {
+        const totalPrice = elementNumber('total-price')
+        const discountAmount = (totalPrice * 20) / 100;
+        const discount = discountAmount.toFixed(2)
+        const discountElement = element('discount')
+        discountElement.innerText = discount
 
-            // subtotal
-            const pay = element('pay')
-            const subTotal = totalPrice - discountAmount
-            pay.innerText = subTotal
-            console.log(pay, subTotal)
-        }
-    })
-    // const discountElement = 
+        // subtotal
+        const pay = element('pay')
+        const subTotal = totalPrice - discountAmount
+        pay.innerText = subTotal
+        console.log(pay, subTotal)
+    }
+})
+
+document.getElementById('go-home').addEventListener('click', function() {
+    const totalPrice = element('total-price')
+    const discount = element('discount')
+    const pay = element('pay')
+
+    // clear element data
+    totalPrice.innerText = '0.00';
+    discount.innerText = '0.00';
+    pay.innerText = '0.00';
+
+    console.log(totalPrice, discount, pay)
+})
 
 
 // utitilies
 function element(id) {
-    const element = document.getElementById(id) 
+    const element = document.getElementById(id)
     return element
 }
 
@@ -84,7 +98,7 @@ function elementNumber(id) {
     const element = document.getElementById(id);
     const elementString = element.innerText
     const number = parseFloat(elementString);
-  
+
     return number
 }
 
